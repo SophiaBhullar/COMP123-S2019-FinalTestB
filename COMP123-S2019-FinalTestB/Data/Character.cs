19ef72924052c38fd7a45ae564f6af97926503ca
SophiaBhullar <bhullarsophia@gmail.com>
2019-08-14 10:33:15 -0400

    added Character and Equipment classes

---
+++ COMP123-S2019-FinalTestB/Data/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2019_FinalTestB.Data
+{
+    class Character
+    {
+        //Character Abilities
+        public string Strength { get; set; }
+        public string Dexterity { get; set; }
+        public string Constitution { get; set; }
+        public string Intelligence { get; set; }
+        public string Wisdom { get; set; }
+        public string Charisma { get; set; }
+
+        //Secondary Abilities
+        public int ArmourClass { get; set; }
+        public int HitPoints { get; set; }
+
+        //Character Class
+        public string CharacterClass { get; set; }
+        public int Level { get; set; }
+
+    }
+}
---
+++ COMP123-S2019-FinalTestB/Objects/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    class Equipment
+    {
+        public string Description { get; set; }
+        public float Weight { get; set; }
+        public int Cost { get; set; }
+    }
+}
