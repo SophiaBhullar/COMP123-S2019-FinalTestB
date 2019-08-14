596c71c534d690d08707214f750b1778177c9830
SophiaBhullar <bhullarsophia@gmail.com>
2019-08-14 12:19:36 -0400

    added menu and tool strip to the character sheet page

--- COMP123-S2019-FinalTestB/Views/CharacterGenerationForm.cs
+++ COMP123-S2019-FinalTestB/Views/CharacterGenerationForm.cs
@@ -6,6 +6,12 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+/* 
+ * STUDENT NAME: SOPHIA BHULLAR
+ * STUDENT ID: 301048344
+ * DESCRIPTION: This is the CharacterGeneratorForm - the main form of the application
+ */
+
 namespace COMP123_S2019_FinalTestB.Views
 {
     public partial class CharacterGenerationForm : MasterForm
@@ -13,6 +19,34 @@ namespace COMP123_S2019_FinalTestB.Views
         public CharacterGenerationForm()
         {
             InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// This is the event handler for the BackButton click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if(MainTabControl.SelectedIndex != 0)
+            {
+                MainTabControl.SelectedIndex--;
+            }
+        }
+
+
+        /// <summary>
+        /// This is the event handler for the NextButton click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if(MainTabControl.SelectedIndex<MainTabControl.TabPages.Count-1)
+            {
+                MainTabControl.SelectedIndex++;
+            }
         }
     }
 }
